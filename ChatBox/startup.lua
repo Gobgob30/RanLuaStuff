b0e9e87ebc17be9da6b1040a78b939a1f055db24
Gobgob30 <97519613+Gobgob30@users.noreply.github.com>
2023-12-04 09:57:39 -0600

    Ahhh

--- ChatBox/startup.lua
+++ ChatBox/startup.lua
@@ -9,12 +9,12 @@ local movement_bool = settings.get("movement_bool", true)
 local base_cp_id = os.getComputerID()
 local function summon_new_sub()
     local cp_id = base_cp_id + sub_z_index
-    commands.setblock("~" .. sub_x_index, "~-1", "~" .. sub_z_index % 15 == 0 and 15 or sub_z_index % 15, "computercraft:computer_command{ComputerId" .. cp_id .. "} destroy")
+    commands.setblock("~" .. sub_x_index, "~-1", "~" .. sub_z_index % 15 == 0 and 15 or sub_z_index % 15, "computercraft:computer_command{ComputerId:" .. cp_id .. "} destroy")
     commands.setblock("~" .. sub_x_index, "~", "~" .. sub_z_index % 15 == 0 and 15 or sub_z_index % 15, "computercraft:wired_modem_full{PeripheralAccess:1b}")
     local disk
     repeat
         commands.setblock("~", "~", "~-1", "computercraft:disk_drive{Item:{id:\"computercraft:computer_normal\",Count:1b,tag:{ComputerId:" .. cp_id .. "}}} destroy")
-        sleep(1)
+        sleep(.1)
         disk = peripheral.wrap("front")
     until disk and disk.isDiskPresent()
     local data_f = fs.open("subs_startup.lua", "r")
@@ -26,14 +26,39 @@ local function summon_new_sub()
     f = fs.open(fs.combine(disk.getMountPath(), ".settings"), "w")
     f.write(textutils.serialize({
         modem_port = sub_z_index + 1,
-        settings.get("owner_id"),
+        owner_id = settings.get("owner_id"),
     }))
     f.close()
     commands.computercraft("turn-on", "#" .. cp_id)
+    commands.setblock("~", "~", "~-1", "air")
+    commands.kill("@e[distance=..5,type=item]")
     if sub_z_index % 15 == 0 then
         sub_x_index = sub_x_index + 1
     end
     sub_z_index = sub_z_index + 1
+    settings.set("sub_index", sub_z_index)
+    settings.set("sub_x_index", sub_x_index)
+    settings.save()
+end
+
+local function check_for_channel(modem)
+    local channel = 1
+    repeat
+        channel = channel + 1
+        modem.transmit(channel, 1, "ready")
+        local event, side, r_channel, replyChannel, message, distance
+        parallel.waitForAny(function()
+            event, side, r_channel, replyChannel, message, distance = os.pullEvent("modem_message")
+        end, function()
+            sleep(.15)
+        end)
+    until message == "ready" or channel > sub_z_index + 1
+    if channel > sub_z_index + 1 then
+        summon_new_sub()
+        sleep(1)
+        return check_for_channel(modem)
+    end
+    return channel
 end
 
 local function run_func(name, func, ...)
@@ -43,17 +68,8 @@ local function run_func(name, func, ...)
         modem = peripheral.find("modem")
     end
     modem.open(1)
-    local channel = 1
-    repeat
-        channel = channel + 1
-        modem.transmit(channel, 1, "ready")
-        local event, side, r_channel, replyChannel, message, distance
-        parallel.waitForAny(function()
-            event, side, r_channel, replyChannel, message, distance = os.pullEvent("modem_message")
-        end, function()
-            sleep(1)
-        end)
-    until message == "ready"
+    local channel = check_for_channel(modem)
+    print("Channel: " .. channel)
     local data = textutils.serialise({
         args = { ... },
         name = name,
@@ -62,30 +78,99 @@ local function run_func(name, func, ...)
     modem.transmit(channel, 1, data)
 end
 
+local defualt_prefixes = { "bios", "edit", "list", "lua", "motd", "paint", "shell", "modem_port" }
+local function is_defualt(name)
+    for i, v in ipairs(defualt_prefixes) do
+        if name:match("^" .. v) then
+            return true
+        end
+    end
+    return false
+end
+
+local banned_settings = {
+    ["GIT_HUB_TOKEN"] = true,
+    ["sub_index"] = true,
+    ["sub_x_index"] = true,
+    ["movement_bool"] = true,
+}
 if not fs.exists("commands") then
     fs.makeDir("commands")
 end
 while true do
-    local event, username, message, uuid, isHidden = os.pullEvent("chat")
-    if not owner_id and message:match("register") then
-        owner_id = uuid
-        settings.set("owner_id", owner_id)
-        settings.save()
-        commands.tellraw(username, { text = "Registered as " .. owner_id, color = "green" })
-    elseif not owner_id then
-        commands.tellraw(username, { text = "Please register first", color = "red" })
-    elseif uuid == owner_id then
-        local args = {}
-        for arg in message:gmatch("%S+") do
-            table.insert(args, arg)
+    parallel.waitForAny(function()
+        local event, username, message, uuid, isHidden = os.pullEvent("chat")
+        if not owner_id and message:match("register") then
+            owner_id = uuid
+            settings.set("owner_id", owner_id)
+            settings.save()
+            commands.tellraw(username, { text = "Registered as " .. owner_id, color = "green" })
+        elseif not owner_id then
+            commands.tellraw(username, { text = "Please register first", color = "red" })
+        elseif uuid == owner_id then
+            if (message:lower() == "cancel" or message:lower() == "stop") then
+                local modem = peripheral.find("modem")
+                for i = 2, sub_z_index do
+                    modem.transmit(i, 1, "suspend")
+                    parallel.waitForAny(function()
+                        while true do
+                            local _, side, channel, replyChannel, message_modem, distance = os.pullEvent("modem_message")
+                            if channel == 1 and replyChannel == i and message_modem == "suspended" then
+                                break
+                            end
+                        end
+                    end, function()
+                        sleep(.15)
+                    end)
+                end
+            else
+                local args = {}
+                for arg in message:gmatch("%S+") do
+                    table.insert(args, arg)
+                end
+                if fs.exists(fs.combine("commands", tostring(args[1]) .. ".lua")) then
+                    local f = fs.open(fs.combine("commands", tostring(args[1]) .. ".lua"), "r")
+                    local data = f.readAll()
+                    f.close()
+                    run_func(username, data, table.unpack(args, 2))
+                else
+                    run_func(username, message, table.unpack(args, 2))
+                end
+            end
         end
-        if fs.exists(fs.combine("commands", tostring(args[1]) .. ".lua")) then
-            local f = fs.open(fs.combine("commands", tostring(args[1]) .. ".lua"), "r")
-            local data = f.readAll()
-            f.close()
-            run_func(username, data)
-        else
-            run_func(username, message)
+    end, function()
+        local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+        if channel == 1 and (message ~= "suspended" or message ~= "ready") then
+            local data = textutils.unserialiseJSON(message)
+            if data then
+                for i, v in ipairs(settings.getNames()) do
+                    if not is_defualt(v) then
+                        settings.unset(v)
+                    end
+                end
+                for i, v in pairs(data) do
+                    if not is_defualt(i) then
+                        settings.set(i, v)
+                    end
+                end
+                settings.save()
+            end
         end
-    end
+    end, function()
+        sleep(1)
+        local modem = peripheral.find("modem")
+        if modem then
+            local names = settings.getNames()
+            local setting = {}
+            for i, v in ipairs(names) do
+                if not is_defualt(v) and not banned_settings[v] then
+                    setting[v] = settings.get(v)
+                end
+            end
+            local data = textutils.serializeJSON(setting)
+            for i = 2, sub_z_index do
+                modem.transmit(i, 1, data)
+            end
+        end
+    end)
 end
--- ChatBox/subs_startup.lua
+++ ChatBox/subs_startup.lua
@@ -1,6 +1,26 @@
+local r = require "cc.require"
+local banned_settings = {
+    ["GIT_HUB_TOKEN"] = true,
+    ["sub_index"] = true,
+    ["sub_x_index"] = true,
+    ["movement_bool"] = true,
+}
+
+local modem = peripheral.find("modem")
+if not modem then
+    error("No modem found", 2)
+end
+local port = settings.get("modem_port") or error("No modem port set", 2)
+modem.open(port)
+if not os.getComputerLabel() then
+    os.setComputerLabel("SubCommandPC")
+end
+
 local function run_func(name, func, ...)
     local args = { ... }
-    commands.gamerule.commandBlockOutput(false)
+    if commands.gamerule.commandBlockOutput() then
+        commands.gamerule.commandBlockOutput(false)
+    end
     parallel.waitForAny(function()
         local ret = { pcall(func, table.unpack(args)) }
         local bool = ret[1]
@@ -18,42 +38,80 @@ local function run_func(name, func, ...)
         end
     end, function()
         while true do
-            local event, username, message, uuid, isHidden = os.pullEvent("chat")
-            if uuid == settings.get("owner_id") and (message:match("cancel") or message:match("c") or message:match("stop") or message:match("s")) then
+            local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+            if channel == port and message == "suspend" then
+                modem.transmit(replyChannel, port, "suspended")
                 break
             end
         end
     end)
-    commands.gamerule.commandBlockOutput(true)
+    if not commands.gamerule.commandBlockOutput() then
+        commands.gamerule.commandBlockOutput(true)
+    end
 end
-local r = require "cc.require"
 
-local modem = peripheral.find("modem")
-if not modem then
-    error("No modem found", 2)
+local defualt_prefixes = { "bios", "edit", "list", "lua", "motd", "paint", "shell", "modem_port" }
+local function is_defualt(name)
+    for i, v in ipairs(defualt_prefixes) do
+        if name:match("^" .. v) then
+            return true
+        end
+    end
+    return false
 end
-local port = settings.get("modem_port") or error("No modem port set", 2)
-modem.open(port)
-os.setComputerLabel(os.getComputerID() .. "_SubCommandPC")
 
 local ready_bool = false
 while true do
     local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-    if channel == port and message == "ready" and not ready_bool then
+    if channel == port and message == "ready" and not ready_bool and replyChannel == 1 then
         ready_bool = true
-        modem.transmit(port, port, "ready")
-    elseif channel == port and ready_bool then
+        modem.transmit(replyChannel, port, "ready")
+        write("Waiting... ")
+    elseif channel == port and ready_bool and replyChannel == 1 then
         ready_bool = false
         local data = textutils.unserialize(message)
         if data then
+            data.func = data.func or "return \"No function provided\""
+            data.name = data.name or "@a"
+            data.args = data.args or {}
             local _env = setmetatable({}, { __index = _ENV })
             _env.require, _env.package = r.make(_env, "/")
             local func, err = load(data.func, data.name, "bt", _env)
             if not func then
                 commands.tellraw(data.name, { text = "Error: " .. err, color = "red" })
             else
-                run_func(data.name, func, table.unpack(data.args))
+                -- run_func(data.name, func, table.unpack(data.args))
+                local ret = { pcall(func, table.unpack(data.args)) }
+                if not ret[1] then commands.tellraw(data.name, { text = "Error: " .. ret[2], color = "red" }) end
+                local setting = {}
+                for k, v in ipairs(settings.getNames()) do
+                    if banned_settings[v] then
+                        settings.unset(v)
+                    end
+                    if not is_defualt(v) then
+                        setting[v] = settings.get(v)
+                    end
+                end
+                modem.transmit(replyChannel, port, textutils.serializeJSON(setting))
             end
+        else
+            write("no data ")
+        end
+        print("Done waiting!")
+    elseif not ready_bool and channel == port and replyChannel == 1 then
+        local setting = textutils.unserializeJSON(message)
+        if setting then
+            for k, v in ipairs(settings.getNames()) do
+                if not is_defualt(v) then
+                    settings.unset(v)
+                end
+            end
+            for k, v in pairs(setting) do
+                if not is_defualt(k) then
+                    settings.set(k, v)
+                end
+            end
+            settings.save()
         end
     end
 end
