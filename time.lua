56eb5379fd619896499ed473c4a6987269e8080d
Gobgob30 <97519613+Gobgob30@users.noreply.github.com>
2024-02-17 00:19:23 -0600

    Ehhhh

--- time.lua
+++ time.lua
@@ -7,18 +7,15 @@ local function stop(time)
     return (time or os.epoch("utc") / 1000) - _time
 end
 
-local format_str = "%H:%M:%S:%3N"
 local function _print(time)
-    print(os.date(format_str, stop(time)))
-end
-
-local function set_format(str)
-    format_str = str
+    local time = stop(time)
+    local hms = os.date("%H:%M:%S", time)
+    local ms = string.format("%03d", time % 1 * 1000)
+    print(hms .. ":" .. ms)
 end
 
 return {
     start = start,
     stop = stop,
     print = _print,
-    set_format = set_format
 }
